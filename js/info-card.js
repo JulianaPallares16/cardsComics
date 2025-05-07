88fd4ede4555b4a50a3d4952dcc2c116ef53c91b
julianap <194113895+JulianaPallares16@users.noreply.github.com.>
2025-05-07 16:11:56 -0500

    style: estructura de la tarjeta terminada

--- js/info-card.js
+++ js/info-card.js
@@ -9,16 +9,34 @@ class InfoCard extends HTMLElement {
         <img id="portada-img" src="" alt="Portada">
         <slot name="clave">[Sin nombre clave]</slot>
         <slot name="casa">[Sin casa]</slot>
+        <slot name="año">[Sin año]</slot>
+        <slot name="info">[Sin información]</slot>
 
 
         `;
         const style = document.createElement('style');
-        shadow.append(container)
+        style.textContent = `
+        div {
+          border: 1px solid #232677;
+          border-radius: 10px;
+          padding: 12px;
+          width: 250px;
+          text-align: center;
+          font-family: sans-serif;
+          background: #fbf7e9;
+          color: #232677;
+        }
+        img {
+          width: 250px;
+          border-radius: 10px 10px 10px 10px;
+        }
+      `;
+        shadow.append(container, style)
     }
     connectedCallback() {
         const slot = this.querySelector('[slot="imagen"]');
         const imgNombre = slot?.textContent?.trim() || 'default.png';
-        const imgElement = this.shadowRoot.querySelector('#profile-img');
+        const imgElement = this.shadowRoot.querySelector('#portada-img');
         imgElement.src = `img/${imgNombre}`;
       }
 }
