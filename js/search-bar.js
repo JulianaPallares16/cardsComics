3c402048e306068d4af16303d7a6abb2ef8b9bbf
Juliana Andrea Pallares Novoa <194113895+JulianaPallares16@users.noreply.github.com>
2025-05-09 14:03:31 +0000

    feat: añadí componente para buscar

--- js/search-bar.js
+++ js/search-bar.js
@@ -1,5 +1,37 @@
 class SearchBar extends HTMLElement {
     constructor() {
         super()
+        this.attachShadow({ mode: 'open'}):
+
+        this.shadowRoot.innerHTML = `
+        <style>
+        input {
+            width:100%;
+            max-width: 800px;
+            padding: 10px;
+            font-size: 22px;
+            border-radius: 5px;
+            border: 2px solid #ccc;
+            margin: 20px auto;
+            display:block;
+            margin-bottom: 40px;
+        }
+        </style>
+        <input type="text" placeholder="Buscar comic...">
+        `
+    }
+    connectedCallback() {
+        const input = this.shadowRoot.querySelector('input');
+        input.addEventListener['input',() => {
+            const search = input.value.toLowerCase();
+            const card = document.querySelectorAll('info-card');
+            card.forEach(cards => {
+                const nombre = cards.getAttritube('nombre','clave').toLowerCase();
+                const ver = nombre.includes(search);
+                cards.style.display = ver ? 'block' : 'none';
+
+            }];
+        };
     }
 }
+customElements.define('search-bar', SearchBar);
